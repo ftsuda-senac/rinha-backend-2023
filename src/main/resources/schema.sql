9718f623260092c8d3958797e686f9d07416709e
ftsuda <fernando@brainyit.com.br>
2023-08-24 09:10:00 -0300

    Ajustes tunning

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,4 +13,15 @@ CREATE TABLE pessoa
     CONSTRAINT uk_apelido UNIQUE (apelido)
 );
 
-CREATE INDEX CONCURRENTLY idx_busca ON pessoa USING gin ((nome || ';' || apelido || ';' || stack_db) gin_trgm_ops);
+-- Create index: https://www.postgresql.org/docs/current/sql-createindex.html
+-- V0 - trimgram com todos os campos + gist (dessa forma o index não é usado nas busca
+-- CREATE INDEX idx_busca ON pessoa USING gist (nome gist_trgm_ops, apelido gist_trgm_ops, stack_db gist_trgm_ops);
+
+-- *** V1 - trimgram concatenando campos + gin (aparentemente tem melhor desempenho)
+-- LEMBRAR DE ALTERAR A QUERY PARA CONCATENAR OS CAMPOS
+CREATE INDEX CONCURRENTLY idx_busca ON pessoa USING gin((nome || ';' || apelido || ';' || stack_db) gin_trgm_ops);
+
+-- V2 - trimgram com campos separados + gin
+-- CREATE INDEX CONCURRENTLY idx_busca_nome ON pessoa USING gin(nome gin_trgm_ops);
+-- CREATE INDEX CONCURRENTLY idx_busca_apelido ON pessoa USING gin(apelido gin_trgm_ops);
+-- CREATE INDEX CONCURRENTLY idx_busca_stack_db ON pessoa USING gin (stack_db gin_trgm_ops);
